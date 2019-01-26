8857f049e96cb5b1fce1c2a1658db4c39be3af8a
gusg21 <gusg21@parkschool.net>
2019-01-26 09:20:20 -0500

    step 0 documentation

--- code.py
+++ code.py
@@ -4,26 +4,32 @@
 import sys
 
 def READ(input):
+    '''Breaks the user's input into tokens'''
     return input
 
 def EVAL(input):
+    '''Evaluates its input (tokens, probably)'''
     return input
 
 def PRINT(input):
+    '''Processes its input for print to the console'''
+
     return input
 
 def repl():
+    '''READ-EVAL-PRINT Loop.'''
+
     print("IS lang REPL\npython version " + str(sys.version_info) + "\n")
 
     done = False
     while not done:
         try:
             inp = input("is]] ")
-        except (KeyboardInterrupt, EOFError):
+        except (KeyboardInterrupt, EOFError): # Break on Ctrl-D or Ctrl-C
             done = True
             break
 
-        if not inp:
+        if not inp: # Break on empty
             done = True
             break
 
